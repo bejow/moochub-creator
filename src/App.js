7150416d4b77da595e72096ab00a162099dda0c0
bejow <ben.wetzel@code.berlin>
2023-05-04 14:40:23 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import validator from '@rjsf/validator-ajv8';
+import schema from './constants/moochub-schema.json'
+import Form from '@rjsf/core';
+import { UiSchema } from '@rjsf/utils';
+
 
 function App() {
+
+  const [formData, setFormData] = React.useState(JSON.parse(localStorage.getItem("formData")) || null);
+  console.log(formData)
+
+  const downloadJsonFile = () => {
+    const element = document.createElement("a");
+    const file = new Blob([JSON.stringify(formData)], { type: 'text/json' });
+    element.href = URL.createObjectURL(file);
+    element.download = "moochubApiExport.json";
+    document.body.appendChild(element); // Required for this to work in FireFox
+    element.click();
+  }
+
+  const onChange = (e) => {
+    localStorage.setItem("formData", JSON.stringify(e.formData));
+    setFormData(e.formData)
+  }
+
+const clearForm = () => {
+  localStorage.removeItem("formData")
+  setFormData(null)
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={clearForm}>Clear Form</button>
+
+      <Form
+        schema={schema}
+        formData={formData}
+        onChange={onChange}
+        validator={validator}
+        onSubmit={downloadJsonFile}
+      />
+      <button onClick={downloadJsonFile}>Export</button>
     </div>
   );
 }
